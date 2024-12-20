6cad993d8bbdf2eb9d26ed0482de9ec5218969a1
Ijaj97 <124039753+Ijaj97@users.noreply.github.com>
2024-12-20 12:54:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
-const express = require('express');
+const express = require("express");
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-const studentRouter = require('./Router/student.router');
+const studentRouter = require("./Router/student.router");
 
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 
 app.use(bodyParser.json());
-app.use(bodyParser.json({limit : "50mb"}));
-app.use(bodyParser.urlencoded({ limit : '50mb', extended : true}));
-app.use(express.static("files"));   
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.static("files"));
 
 // ================================================== //
 
-app.use('/api/student', studentRouter);
+app.use("/api/student", studentRouter);
 
-app.get('/', (req, res) => {
-    res.send('Welcome');
+app.get("/", (req, res) => {
+  res.send("Welcome");
 });
 
 app.listen(3050, () => {
-    console.log('Port No. 3050');
+  console.log("Port No. 3050");
 });
 
 // mongoose.connect('mongodb://127.0.0.1:27017/Backend').then(res => {
 //     console.log('DB connected');
 // });
 
-mongoose.connect('mongodb+srv://ijajjaman29:4W5tm5HAZiMKQUio@studentdata.mfu5i.mongodb.net/').then(res => {
-    console.log('DB connected');
-});
-
+mongoose
+  .connect(
+    "mongodb+srv://ijajjaman29:4W5tm5HAZiMKQUio@studentdata.mfu5i.mongodb.net/",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      tls: true,
+      tlsAllowInvalidCertificates: true,
+    }
+  )
+  .then((res) => {
+    console.log("DB connected");
+  });
